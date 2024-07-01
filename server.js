56da0acde3bd3927c8c62997613b151c5766fec8
omozuas <iyanuomozua@gmail.com>
2024-07-01 18:56:28 +0100

    new

--- server.js
+++ server.js
@@ -7,17 +7,17 @@ const cron = require("node-cron");
 
 
 
-// cron.schedule("*/3 * * * * ", async () => {
-//     try {
-//       const currentTime = new Date();
-//       console.log(`Current time: ${currentTime}`);
+cron.schedule("*/3 * * * * ", async () => {
+    try {
+      const currentTime = new Date();
+      console.log(`Current time: ${currentTime}`);
       
-//       const response = await axios.get('https://shope-express.onrender.com/');
-//       console.log('API Response:', response.data);
-//     } catch (error) {
-//       console.error('Error fetching API:', error);
-//     }
-//   });
+      const response = await axios.get('https://weather-location-ea5a.onrender.com/weather');
+      console.log('API Response:', response.data);
+    } catch (error) {
+      console.error('Error fetching API:', error);
+    }
+  });
 
 // Set the view engine to EJS
 app.set("view engine", "ejs");
@@ -25,29 +25,32 @@ app.set("view engine", "ejs");
 // Serve the public folder as static files
 app.use(express.static("public"));
 
-// Render the index template with default values for weather and error
+
 app.get("/", (req, res) => {
   res.render("index", { weather: null, error: null ,greeting:null,clientIp:null});
 });
 app.get("/weather", (req, res) => {
     res.send("hello from weather app",);});
-// Middleware to extract client IP address
+
 app.set('trust proxy', true);
 
-app.get('/api/hello', async (req, res) => {
-    const visitorName = req.query.visitor_name || 'Guest';
-    const location1 = req.query.location|| 'london';
+app.get('/api/get-temprature', async (req, res) => {
+    const visitorName = req.query.visitor_name ;
+    const location1 = req.query.location;
    let clientIp ;
     let error = null;
     let greeting;
-    let temperature;
     let weather
+    if(!visitorName){
+        error = 'Failed to fetch weather data'
+    }
+
     try {
-        // Get weather data for New York
+       
         const weatherResponse = await axios.get(`https://api.openweathermap.org/data/2.5/weather?id=2172797`, {
             params: {
                 q: location1,
-                
+                units: 'metric',
                 appid:process.env.OPENWEATHERMAP_API_KEY
             }
         });
